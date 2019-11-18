9cbb253e5756d53535a286ec3132e2d16073f363
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-11-18 10:14:44 -0800

    added method to level

--- PixelPhotoLeveler/Program.cs
+++ PixelPhotoLeveler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PixelPhotoLeveler
 {
@@ -6,9 +7,47 @@ namespace PixelPhotoLeveler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("To which drive is your phone mounted?");
-            string drive = Console.ReadLine();
-            Console.WriteLine(drive);
+            Console.WriteLine("What is the path to the folder you would like leveled?");
+            string parent = Console.ReadLine();
+            Console.WriteLine("Moving all files into parent directory...");
+
+            LevelFolder(parent, parent);
+
+            Console.WriteLine("Done");
+        }
+
+        public static void LevelFolder(string parent, string subDir = "")
+        {
+            string[] files = Directory.GetFiles(subDir);
+            string[] subDirectories;
+
+            if (subDir == "")
+            {
+                subDirectories = Directory.GetDirectories(parent);
+            } else
+            {
+                subDirectories = Directory.GetDirectories(subDir);
+            }
+
+            // recursively running on each subfolder to bring all files up
+            foreach (string dir in subDirectories)
+            {
+                LevelFolder(parent, dir);
+            }
+
+            if (subDir != "")
+            {
+                // move each file into parent directory
+                foreach (string _file in files)
+                {
+                    string filename = _file.Replace($"{subDir}\\", "");
+                    File.Move(_file, $"{parent}\\{filename}");
+                    Console.WriteLine($"Moved file - {filename}");
+                }
+
+                Directory.Delete(subDir);
+                Console.WriteLine($"Deleted folder - {Path.GetFileName(subDir)}");
+            }
         }
     }
 }
